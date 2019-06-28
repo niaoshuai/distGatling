d5765bd378ef0d3696b78b07c24161086557d433
niaoshuai <niao.shuai123@163.com>
2019-06-28 15:25:17 +0800

    Test

--- gatling-commons/src/test/java/com/walmart/gatling/commons/JarExecutorTest.java
+++ gatling-commons/src/test/java/com/walmart/gatling/commons/JarExecutorTest.java
@@ -17,11 +17,13 @@ public class JarExecutorTest {
     @Test
     public void testGetScriptCommand(){
         CommandLine cmdLine = new CommandLine("/home/niaoshuai/gatling-charts-highcharts-bundle-3.0.2/bin/gatling.sh");
-        cmdLine.addArgument("-sf").addArgument("/home/niaoshuai/agentTmp");
+        cmdLine.addArgument("-sf").addArgument("/home/niaoshuai/agentTmp/bd3cf531-75ef-47b0-b185-951a0e2b4600/simulation");
         cmdLine.addArgument("-s").addArgument("computerdatabase.BasicSimulation");
-        Map<String, String> substitutionMap = new HashMap<>(2);
-        substitutionMap.put("JAVA_OPTS","-Dusers=10 -Dramp=100");
-        cmdLine.setSubstitutionMap(substitutionMap);
+        cmdLine.addArgument("-rf").addArgument("/home/niaoshuai/gatling-charts-highcharts-bundle-3.0.2/public/bd3cf531-75ef-47b0-b185-951a0e2b4600")
+                .addArgument("-nr");
+//        Map<String, String> substitutionMap = new HashMap<>(2);
+//        substitutionMap.put("JAVA_OPTS","-Dusers=10 -Dramp=100");
+//        cmdLine.setSubstitutionMap(substitutionMap);
 
         DefaultExecutor executor = new DefaultExecutor();
         try {
